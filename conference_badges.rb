07997bb690e81b1d869918108952b24da2f700a5
Julian Brown <julianxbrown@gmail.com>
2018-11-11 20:22:51 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,3 +5,19 @@ names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 def badge_maker(name) 
   puts "Hello, my name is #{names}."
 end
+
+names.each do |name|
+greet(name)
+end
+
+def batch_badge_creator(names) 
+  
+  
+  
+end
+
+def assign_rooms (names)
+  
+  
+  
+end 
